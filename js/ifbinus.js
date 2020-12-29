7a715f3d823344493f55865b5d81cc7eab473106
IgnatiusMD <michaeldinata33@gmail.com>
2020-12-30 01:20:25 +0700

    lagi

--- js/ifbinus.js
+++ js/ifbinus.js
@@ -6,11 +6,11 @@ function myFunction(){
     var data = document.getElementById("data");
   
     // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
-      text.style.display = "block";
-      data.style.display = "none";
-    } else {
-      text.style.display = "none";
-      data.style.display = "block";
+    if(checkBox.checked == true){
+        text.style.display = "block";
+        data.style.display = "none";
+    }else{
+        text.style.display = "none";
+        data.style.display = "block";
     }
-  }
+}
